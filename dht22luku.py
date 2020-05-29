48ad107d9fe7674774333ccd666dbabcfd24bb62
Jari Hiltunen <65418152+divergentti@users.noreply.github.com>
2020-05-29 18:06:11 +0300

    Update dht22luku.py

--- dht22luku.py
+++ dht22luku.py
@@ -1,8 +1,10 @@
 # suorita python-komennolla!
+# jos CRON:ssa, ps -A ja kill prosessi ensin
 import paho.mqtt.client as mqtt #mqtt kirjasto
 import time
 import sys
 import Adafruit_DHT #adafruit-kirjasto
+import syslog #kirjoitetaan virheet syslogiin
 
 mqttanturi = mqtt.Client("kanalasisa-dht22") #mqtt objektin luominen
 mqttanturi.username_pw_set("kayttaja","salari") #mqtt useri ja salari
@@ -15,21 +17,23 @@ kanala_dht22_kosteus = "kanala/sisa/kosteus"
 while True:
     try:
         kosteus22, lampo22 = Adafruit_DHT.read_retry(22, 4) #22 on sensorin tyyppi, 4 on GPIO pinni, ei fyysinen pinni
-       
         if lampo22 is not None:
             lampo22 =  '{:.1f}'.format(lampo22)
             # print ("Lampo: ") + str(lampo22)
             mqttanturi.publish(kanala_dht22_lampo, payload=lampo22, retain=True)
         else:
-	 print (time.strftime("%H:%M:%S ") + "Lampotilatietoa ei saatavilla")
+         #kirjoitetaan syslogiin virhe
+	 syslog.syslog (time.strftime("%H:%M:%S ") + "Lampotilatietoa ei saatavilla")
+         print (time.strftime("%H:%M:%S ") + "Lampotilatietoa ei saatavilla")
 	if kosteus22 is not None:
-            kosteus22 = '{:.1f}'.format(kosteus22)
+           kosteus22 = '{:.1f}'.format(kosteus22)
            # print ("Kosteus: ") + str(kosteus22)
-            mqttanturi.publish(kanala_dht22_kosteus, payload=kosteus22, retain=True)
+           mqttanturi.publish(kanala_dht22_kosteus, payload=kosteus22, retain=True)
 	else:
-	 print (time.strftime("%H:%M:%S ") + "Kosteustietoa ei saatavilla")        
-
-        time.sleep(600)
+         #kirjoitetaan syslogiin virhe
+	 syslog.syslog (time.strftime("%H:%M:%S ") + "Kosteustietoa ei saatavilla")
+         print (time.strftime("%H:%M:%S ") + "Kosteustietoa ei saatavilla")
+        time.sleep(60)
 
     except (EOFError, SystemExit, KeyboardInterrupt):
         mqttanturi.disconnect()
