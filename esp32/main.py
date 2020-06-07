5c9be6816e09e7bb0328841c6fb10896c0ce7c5d
Jari Hiltunen <65418152+divergentti@users.noreply.github.com>
2020-06-07 17:58:42 +0300

    Update main.py

--- esp32/main.py
+++ esp32/main.py
@@ -11,8 +11,8 @@ from parametrit import CLIENT_ID, MQTT_SERVERI, MQTT_PORTTI, MQTT_KAYTTAJA, \
   MQTT_SALASANA, DHT22_LAMPO, DHT22_KOSTEUS, RELE_OHJAUS, PINNI_NUMERO, \
   DHT22_LAMPO_KORJAUSKERROIN, DHT22_KOSTEUS_KORJAUSKERROIN
 
-#anturi = dht.DHT22(Pin(PINNI_NUMERO))
-anturi = dht.DHT22(machine.Pin(4))
+anturi = dht.DHT22(Pin(PINNI_NUMERO))
+#anturi = dht.DHT22(machine.Pin(4))
 
 def rele_tila(rele_ohjaus, msg):
   print((rele_ohjaus, msg))
@@ -39,6 +39,7 @@ def tallenna_lampo_kosteus_tiedot():
     anturi.measure()
   except OSError as e:
     print("Sensoria ei voida lukea!")
+    vilkuta_ledi(5)
   lampo = anturi.temperature() * DHT22_LAMPO_KORJAUSKERROIN
   kosteus = anturi.humidity() * DHT22_KOSTEUS_KORJAUSKERROIN
   print('Lampo: %3.1f C' % lampo)
@@ -52,14 +53,17 @@ def tallenna_lampo_kosteus_tiedot():
     client.connect()
   except OSError as e:
     print("Yhteys mqtt-palvelimeen ei onnistunut!")
+    vilkuta_ledi(5)
   try:
     client.publish(DHT22_LAMPO, str(lampo))
   except OSError as e:
     print("Arvoa %s ei voida julkistaa! " % str(lampo))
+    vilkuta_ledi(5)
   try:
     client.publish(DHT22_KOSTEUS, str(kosteus))
   except OSError as e:
     print("Arvoa %s ei voida julkistaa! " % str(kosteus))
+    vilkuta_ledi(5)
   vilkuta_ledi(1)
   print('Yhdistetty %s MQTT brokeriin, tallennettu %s %s' % (MQTT_SERVERI, lampo, kosteus))
   time.sleep(60)
