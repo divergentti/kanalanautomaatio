957d5c826d858f3e5d6ff272c40338ee7e08584b
Jari Hiltunen <65418152+divergentti@users.noreply.github.com>
2020-06-07 13:30:53 +0300

    Ledi mukaan WiFi-statukseen

--- esp32/boot.py
+++ esp32/boot.py
@@ -13,63 +13,39 @@ from time import sleep
 from parametrit import SSID1, SSID2,SALASANA1,SALASANA2
 
 wificlient_if = network.WLAN(network.STA_IF)
-wificlient_if.active(True)
+wificlient_if.active(False)
 
-def yhdista_ssid1():
-  print("...")
-  print("Kaynnistetaan WiFi ...")
+def yhdista_wifi(ssid_nimi, salasana):
 
-  _ = wificlient_if.active(True)
-  _ = wificlient_if.connect(SSID1, SALASANA1)
-
-  tmo = 50
-
-  while not wificlient_if.isconnected():
-      utime.sleep_ms(100)
-      tmo -= 1
-      if tmo == 0:
-          break
-
-  if tmo > 0:
-      ifcfg = wificlient_if.ifconfig()
-      print("WiFi yhdistetty, IP:", ifcfg[0])
-      utime.sleep_ms(500)
-
-
-def yhdista_ssid2():
-  print("...")
-  print("Kaynnistetaan WiFi ...")
-
-  _ = wificlient_if.active(True)
-  _ = wificlient_if.connect(SSID2, SALASANA2)
-
-  tmo = 50
-
-  while not wificlient_if.isconnected():
-      utime.sleep_ms(100)
-      tmo -= 1
-      if tmo == 0:
-          break
-
-  if tmo > 0:
-      ifcfg = wificlient_if.ifconfig()
-      print("WiFi yhdistetty, IP:", ifcfg[0])
-      utime.sleep_ms(500)
+    vilkuta_ledi(1)
+    wificlient_if = network.WLAN(network.STA_IF)
+    print("Kokeillaan %s" %ssid_nimi)
+    wificlient_if.active(True)
+    wificlient_if.connect(ssid_nimi, salasana)
+    time.sleep(3)
+    if wificlient_if.isconnected():
+        vilkuta_ledi(2)
+        print('Verkon kokoonpano:', wificlient_if.ifconfig())
+    return True
 
 def ei_voida_yhdistaa():
   print("Yhteys ei onnistu. Bootataan 10 s. kuluttua")
-  sleep(10)
-  #machine.reset()
+  vilkuta_ledi(10)
+  sleep(5)
+  machine.reset()
 
-while not wificlient_if.isconnected():
+def vilkuta_ledi(kertaa):
+    ledipinni = machine.Pin(2, machine.Pin.OUT)
+    for i in range(kertaa):
+        ledipinni.on()
+        utime.sleep_ms(100)
+        ledipinni.off()
+        utime.sleep_ms(100)
 
-    try:
-        yhdista_ssid1()
-    except OSError as e:
-        yhdista_ssid2()
-    finally:
-        print('Yhteydet kunnossa. Jatketaan')
-        break
+if yhdista_wifi(SSID1, SALASANA1):
+    print("Jatketaan %s verkon kanssa" %SSID1)
+elif yhdista_wifi(SSID2, SALASANA2):
+    print("Jatketaan %s verkon kanssa" % SSID2)
+else:
+    ei_voida_yhdistaa()
 
-while False:
-  ei_voida_yhdistaa()
