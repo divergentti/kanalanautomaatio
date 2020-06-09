64485e3c31dd4ca047c2941a812905ef85634fba
Jari Hiltunen <65418152+divergentti@users.noreply.github.com>
2020-06-09 15:03:48 +0300

    Multitaskausta ja koodin uudelleenorganisointia

--- esp32/main.py
+++ esp32/main.py
@@ -1,3 +1,11 @@
+#tarkoitettu ESP32-Wroom-32 NodeMCU:lle
+#Lukee DHT22-anturia ja ohjaa 2-kanavaista reletta
+#Parametrit tuodaan parametrit.py-tiedostosta
+#Yksi ledin vilaus = toiminta alkaa
+#Kaksi ledin vilautusta = toiminta saatettu loppuun
+#10 ledin vilautusta = virhe!
+#Osa kanalan automaatioprojektia, https://hiltsu.dy.fi
+#Jari Hiltunen 9.6.2020
 import time
 import utime
 import machine
@@ -6,16 +14,31 @@ import dht
 from machine import Pin
 from umqttsimple import MQTTClient
 
-#tuodaan parametrit
+#tuodaan parametrit tiedostosta parametrit.py
 from parametrit import CLIENT_ID, MQTT_SERVERI, MQTT_PORTTI, MQTT_KAYTTAJA, \
   MQTT_SALASANA, DHT22_LAMPO, DHT22_KOSTEUS, RELE_OHJAUS, PINNI_NUMERO, \
   DHT22_LAMPO_KORJAUSKERROIN, DHT22_KOSTEUS_KORJAUSKERROIN, \
   RELE1PINNI, RELE2PINNI
-
+#dht-kirjasto tukee muitakin antureita kuin dht22
 anturi = dht.DHT22(Pin(PINNI_NUMERO))
 #virhelaskurin idea on tuottaa bootti jos jokin menee pieleen liian usein
-virhe = 0
+anturivirhe = 0
+relevirhe = 0
 client = MQTTClient(CLIENT_ID, MQTT_SERVERI, MQTT_PORTTI, MQTT_KAYTTAJA, MQTT_SALASANA)
+
+def mqtt_palvelin_yhdista():
+  try:
+    client.connect()
+  except OSError as e:
+    print("Ei voida yhdistaa! ")
+    client.disconnect()
+    time.sleep(60)
+    restart_and_reconnect()
+    return False
+  # releen ohjaus
+  client.set_callback(rele_tila)
+  client.subscribe(RELE_OHJAUS)
+  return True
 
 def rele_tila(rele_ohjaus, msg):
   # Huom! Releet kytketty NC (Normally Closed) jolloin 0 = on
@@ -29,6 +52,8 @@ def rele_tila(rele_ohjaus, msg):
   #2 = molemmat on
   #3 = rele 1 off, rele 2 on
   print((rele_ohjaus, msg))
+  # testataan onko tullut uusi arvo vai ei
+
   if rele_ohjaus == RELE_OHJAUS and msg == b'0':
     print('Laita kaikki releet off')
     rele1.value(1)
@@ -48,62 +73,58 @@ def rele_tila(rele_ohjaus, msg):
   vilkuta_ledi(2)
   time.sleep(1)
 
+def lue_releen_status():
+  global relevirhe
+  vilkuta_ledi(1)
+  print('Tarkistetaan releen ohjaustietoa...')
+  try:
+    client.check_msg()
+  except OSError as e:
+    print("Releviestin lukuvirhe!")
+    relevirhe = relevirhe + 1
+    return False
+  vilkuta_ledi(2)
+  relevirhe = 0
+  return True
+
 
 def lue_lampo_ja_yhdista():
-  global virhe
+  global anturivirhe
   vilkuta_ledi(1)
   try:
     anturi.measure()
   except OSError as e:
     print("Sensoria ei voida lukea!")
-    virhe = virhe + 1
+    anturivirhe = anturivirhe + 1
     return False
   lampo = anturi.temperature() * DHT22_LAMPO_KORJAUSKERROIN
   kosteus = anturi.humidity() * DHT22_KOSTEUS_KORJAUSKERROIN
   print('Lampo: %3.1f C' % lampo)
   print('Kosteus: %3.1f %%' % kosteus)
-  print("Yhdistetaan mqtt-palvelimeen %s ..." %MQTT_SERVERI)
-  client = MQTTClient(CLIENT_ID, MQTT_SERVERI, MQTT_PORTTI, MQTT_KAYTTAJA, MQTT_SALASANA)
-
-  try:
-    client.connect()
-    #yhdistetaan joka kerran, tulee socket error mosquittoon
-  except OSError as e:
-    print("Ei voida yhdistaa! ")
-    virhe = virhe + 1
-    return False
-  # releen ohjaus
-  client.set_callback(rele_tila)
-  client.subscribe(RELE_OHJAUS)
-
+  print("Tallenntaan arvot mqtt-palvelimeen %s ..." %MQTT_SERVERI)
   lampo='{:.1f}'.format(lampo)
   kosteus = '{:.1f}'.format(kosteus)
 
   try:
     client.publish(DHT22_LAMPO, str(lampo))
   except OSError as e:
-    print("Arvoa %s ei voida julkistaa! " % str(lampo))
-    virhe = virhe + 1
+    print("Arvoa %s ei voida tallentaa! " % str(lampo))
+    anturivirhe = anturivirhe + 1
     return False
   try:
     client.publish(DHT22_KOSTEUS, str(kosteus))
   except OSError as e:
-    print("Arvoa %s ei voida julkistaa! " % str(kosteus))
-    virhe = virhe + 1
+    print("Arvoa %s ei voida tallentaa! " % str(kosteus))
+    anturivirhe = anturivirhe + 1
     return False
-  print('Yhdistetty %s MQTT brokeriin, tallennettu %s %s' % (MQTT_SERVERI, lampo, kosteus))
-  print('Tarkistetaan ohjaustietoa...')
-  client.check_msg()
-  vilkuta_ledi(2)
-  print("Odotetaan seuraavaa arvoa...")
-  time.sleep(60)
-  virhe = 0
+  print('Tallennettu %s %s' % (lampo, kosteus))
+  anturivirhe = 0
   return True
 
 
 def restart_and_reconnect():
   print('Ongelmia. Boottaillaan ...')
-  vilkuta_ledi(5)
+  vilkuta_ledi(10)
   time.sleep(5)
   machine.reset()
   #resetoidaan
@@ -116,14 +137,45 @@ def vilkuta_ledi(kertaa):
     ledipinni.off()
     utime.sleep_ms(100)
 
-while virhe < 5:
-  print("Virhelaskuri: %s" % virhe)
+def anturiluuppi():
+ while anturivirhe < 5:
+  print("Anturiluupin virhelaskuri: %s" % anturivirhe)
   try:
     lue_lampo_ja_yhdista()
   except OSError as e:
-    print("Virhelaskuri: %s" % virhe)
-  #tallenna_lampo_kosteus_tiedot()
+    print("Anturiluupin virhelaskuri: %s" % anturivirhe)
+    # Virheita liikaa
+    restart_and_reconnect()
+  time.sleep(20)
+  yield None
 
+def releluuppi():
+ while relevirhe < 5:
+  print("Releloopin virhelaskuri: %s" % relevirhe)
+  try:
+    lue_releen_status()
+  except OSError as e:
+    print("Releloopin virhelaskuri: %s" % relevirhe)
+    # Virheita liikaa
+    restart_and_reconnect()
+  time.sleep(10)
+  yield None
 
-#Virheita liikaa
+#annetaan verkon tasaantua par isekkaa
+time.sleep(2)
+
+try:
+  mqtt_palvelin_yhdista()
+except OSError as e:
+  print("Ei onnistunut yhteys mqtt-palvelimeen %s" % MQTT_SERVERI)
+  restart_and_reconnect()
+
+TehtavaJono = [anturiluuppi(), releluuppi()]
+
+while True:
+  # loopataan ellei virheita muodostu
+  for task in TehtavaJono:
+    next(task)
+
+# Jos kaikki menee pieleen
 restart_and_reconnect()
