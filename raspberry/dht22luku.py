5b48d7d1a1f2fbbaab62e845e85f507040633507
Jari Hiltunen <65418152+divergentti@users.noreply.github.com>
2020-06-24 10:33:22 +0300

    Lisätty arvojen oikeellisuustarkistus

--- raspberry/dht22luku.py
+++ raspberry/dht22luku.py
@@ -1,9 +1,15 @@
 # !/usr/bin/python3
-# jos CRON:ssa, ps -A ja kill prosessi ensin!
-# 10.6.2020 Jari Hiltunen
+# 24.6.2020 Jari Hiltunen
 import paho.mqtt.client as mqtt # tuodaan mqtt kirjasto
 # asennus pip3 install paho-mqtt
 import time
+# uuden adafruitin kanssa tarvittaisiin muiden komponenttien asennus:
+# pip3 install RPI.GPIO
+# pip3 install adafruit-blinka - ei tarvita
+# pip3 install adafruit-circuitpython-dht  ei tarvita
+# sudo apt-get install libgpiod2 ei tarvita
+#import board
+# import adafruit_dht ei tarvita
 import Adafruit_DHT # vanha toimiva adafruit-kirjasto
 # muuttujat tuodaan parametrit.py_tiedostosta
 from parametrit import ANTURINIMI, MQTTKAYTTAJA, MQTTSALARI, MQTTSERVERI, MQTTSERVERIPORTTI, \
@@ -13,22 +19,25 @@ mqttanturi = mqtt.Client(ANTURINIMI)   # mqtt objektin luominen
 mqttanturi.username_pw_set(MQTTKAYTTAJA, MQTTSALARI) # mqtt useri ja salari
 mqttanturi.connect(MQTTSERVERI, port=MQTTSERVERIPORTTI, keepalive=60) # Yhteys brokeriin
 mqttanturi.loop_start() # Loopin kaynnistys
-# dhtLaite = adafruit_dht.DHT22(DHT22PINNI) # tarpeeton DHT-anturiobjekti
+# dhtLaite = adafruit_dht.DHT22(DHT22PINNI) #DHT-anturiobjekti
 
 while True:
     try:
-       
-        kosteus, lampo = Adafruit_DHT.read_retry(22, DHT22PINNI)
-        if lampo is not None:
+        #kosteus = dhtLaite.humidity
+        #lampo = dhtLaite.temperature
+        kosteus, lampo = Adafruit_DHT.read_retry(22, DHT22PINNI) 
+        if (lampo is not None) and (lampo < 100) and (lampo > -40):
+            # estetaan vaarat arvot
             lampo ='{:.1f}'.format(lampo)
             print("Lampo: %s" % str(lampo))
             mqttanturi.publish(AIHELAMPO, payload=lampo, retain=True)
         else:
-         print (time.strftime("%H:%M:%S ") + "Lampotilatietoa ei saatavilla")
-        if kosteus is not None:
-           kosteus = '{:.1f}'.format(kosteus)
-           print("Kosteus: %s" % str(kosteus))
-           mqttanturi.publish(AIHEKOSTEUS, payload=kosteus, retain=True)
+            print (time.strftime("%H:%M:%S ") + "Lampotilatietoa ei saatavilla!")
+        if (kosteus is not None) and (kosteus >0) and (kosteus < 101):
+            # estetaan vaarat arvot
+            kosteus = '{:.1f}'.format(kosteus)
+            print("Kosteus: %s" % str(kosteus))
+            mqttanturi.publish(AIHEKOSTEUS, payload=kosteus, retain=True)
         else:
             print(time.strftime("%H:%M:%S ") + "Kosteustietoa ei saatavilla")
         time.sleep(LUKUVALI) # lukufrekvenssi
