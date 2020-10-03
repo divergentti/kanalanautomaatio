1fc65203f387fa313b154d6021409e11cca8383a
Jari Hiltunen <65418152+divergentti@users.noreply.github.com>
2020-10-03 12:36:46 +0300

    Update parametrit.py

--- esp32-mq135/parametrit.py
+++ esp32-mq135/parametrit.py
@@ -7,6 +7,7 @@ MQTT_PORTTI = '1883'
 MQTT_KAYTTAJA = 'user'
 MQTT_SALASANA = 'sala'
 CLIENT_ID = "ESP32-kaasuanturi"
+DHCP_NIMI = "ESP32-kaasuanturi"
 MQ135_PINNI = 36
 SISA_LAMPO = b'kanala/sisa/lampo'
 SISA_KOSTEUS = b'kanala/sisa/kosteus'
