541d1cad666d174fde3dfcff0278271677bdd166
Jari Hiltunen <65418152+divergentti@users.noreply.github.com>
2021-01-08 11:30:05 +0200

    Now limiter switch and distance tested
    Does not block WebREPL either

--- esp32-hcr-04-steppermotor4curtain/main.py
+++ esp32-hcr-04-steppermotor4curtain/main.py
@@ -1,9 +1,12 @@
 """" This script controls inside curtain in the chicken house (or anything what 5V stepper motor can handle).
 
 Operation basics: measures distance from two HCSR-04 sensors and if distance is > 30 cm, opens the curtain, else
-closes the curtain.
+closes the curtain. Limiter switch is for the upmost position detection.
 
-Installation with ESP32 Dev Board: connect pins as in the parameters.py and for the echo-pin use either 
+Operation starts by rolling wire to the pulley from the lowest position. That value will be used for up - down steps
+counter.
+
+Installation with ESP32 Dev Board: connect pins as in the parameters.py and for the echo-pin use either
   voltage splitter or level converter from 5V to 3.3V.
 
 Stepper motor: 28BYJ-48, control board ULN2003.
@@ -19,6 +22,7 @@ Libraries:
  Stepper.py https://github.com/IDWizard/uln2003/blob/master/uln2003.py
 
 14.12.2020 Jari Hiltunen
+8.1.2021 Added limiter switch and fixed distance measuring so that WebREPL works
 """
 
 
@@ -31,11 +35,13 @@ from MQTT_AS import MQTTClient, config
 import network
 from hcsr04 import HCSR04
 
+
 try:
     f = open('parameters.py', "r")
     from parameters import SSID1, SSID2, PASSWORD1, PASSWORD2, MQTT_SERVER, MQTT_PASSWORD, MQTT_USER, MQTT_PORT, \
         CLIENT_ID, BATTERY_ADC_PIN, TOPIC_ERRORS, STEPPER1_PIN1, STEPPER1_PIN2, STEPPER1_PIN3, STEPPER1_PIN4, \
-        STEPPER1_DELAY, HCSR04_1_ECHO_PIN, HCSR04_1_TRIGGER_PIN, HCSR04_2_TRIGGER_PIN, HCSR04_2_ECHO_PIN
+        STEPPER1_DELAY, HCSR04_1_ECHO_PIN, HCSR04_1_TRIGGER_PIN, HCSR04_2_TRIGGER_PIN, HCSR04_2_ECHO_PIN, \
+        LIMITER_SWITCH_PIN
 except OSError:  # open failed
     print("parameter.py-file missing! Can not continue!")
     raise
@@ -76,11 +82,26 @@ class StepperMotor:
         self.full_rotation = int(4075.7728395061727 / 8)  # http://www.jangeox.be/2013/10/stepper-motor-28byj-48_25.html
         self.uplimit = 0
         self.toplimit = 500
-        self.curtain_steps = 300
-        self.up_down_delay = 5
+        self.curtain_steps = 0
+        self.up_down_delay_ms = 500
         self.curtain_up = False
         self.curtain_up_time = None
         self.curtain_down_time = None
+
+    async def find_top_position(self):
+        print("Starting uprotation...")
+        n = 0
+        starttime = utime.ticks_ms()
+        while (self.curtain_up is False) and ((utime.ticks_ms() - starttime) < 30000):
+            if limiter_switch.value() == 0:
+                self.motor.step(1, -1)
+                n += 1
+            else:
+                self.up_down_delay_ms = utime.ticks_ms() - starttime
+                self.curtain_steps = n
+                self.curtain_up = True
+                await self.zero_to_position()
+                print("Found top steps %s" % self.curtain_steps)
 
     async def turn_x_degrees_right(self, degrees):
         self.motor.angle(degrees)
@@ -94,19 +115,19 @@ class StepperMotor:
     async def turn_x_steps_left(self, steps):
         self.motor.step(steps, -1)
 
-    async def zero_to_potision(self):
+    async def zero_to_position(self):
         self.motor.reset()
 
     async def roll_curtain_up(self):
         self.motor.step(self.curtain_steps)
         self.curtain_up = True
-        await asyncio.sleep(self.up_down_delay)
+        await asyncio.sleep(self.up_down_delay_ms)
         self.curtain_up_time = utime.localtime()
 
     async def roll_curtain_down(self):
         self.motor.step(self.curtain_steps, -1)
         self.curtain_up = False
-        await asyncio.sleep(self.up_down_delay)
+        await asyncio.sleep(self.up_down_delay_ms)
         self.curtain_down_time = utime.localtime()
 
 
@@ -119,49 +140,35 @@ class DistanceSensor:
 
     async def measure_distance_cm(self):
         try:
-            self.distancecm = self.sensor.distance_cm()
-            if self.distancecm > 0:
-                return self.distancecm
+            distance = self.sensor.distance_cm()
+            if distance > 0:
+                self.distancecm = distance
+                await asyncio.sleep_ms(100)
             else:
-                self.distancecm = 0
-                return self.distancecm
+                self.distancecm = None
         except OSError as ex:
             print('ERROR getting distance:', ex)
 
     async def measure_distance_cm_loop(self):
         while True:
-            try:
-                self.distancecm = self.sensor.distance_cm()
-                if self.distancecm > 0:
-                    pass
-                else:
-                    self.distancecm = 0
-            except OSError as ex:
-                print('ERROR getting distance:', ex)
-            await asyncio.sleep_ms(1)
+            await self.measure_distance_cm()
+            await asyncio.sleep_ms(100)
 
     async def measure_distance_mm(self):
         try:
-            self.distancemm = self.sensor.distance_mm()
-            if self.distancemm > 0:
-                return self.distancemm
+            distance = self.sensor.distance_mm()
+            if distance > 0:
+                self.distancemm = distance
+                await asyncio.sleep_ms(100)
             else:
-                self.distancemm = 0
-                return self.distancemm
+                self.distancemm = None
         except OSError as ex:
             print('ERROR getting distance:', ex)
 
     async def measure_distance_mm_loop(self):
         while True:
-            try:
-                self.distancemm = self.sensor.distance_mm()
-                if self.distancemm > 0:
-                    pass
-                else:
-                    self.distancemm = 0
-            except OSError as ex:
-                print('ERROR getting distance:', ex)
-            await asyncio.sleep_ms(1)
+            await self.measure_distance_mm()
+            await asyncio.sleep_ms(100)
 
 
 async def resolve_date():
@@ -193,16 +200,18 @@ async def mqtt_report():
                 await client.publish(AIHE_CO2, str(kaasusensori.eCO2_keskiarvo), retain=False, qos=0) """
 
 
-reel_motor = StepperMotor(STEPPER1_PIN1, STEPPER1_PIN2, STEPPER1_PIN3, STEPPER1_PIN4, STEPPER1_DELAY)
+pulley_motor = StepperMotor(STEPPER1_PIN1, STEPPER1_PIN2, STEPPER1_PIN3, STEPPER1_PIN4, STEPPER1_DELAY)
 inside_distance = DistanceSensor(HCSR04_1_TRIGGER_PIN, HCSR04_1_ECHO_PIN)
 outside_distance = DistanceSensor(HCSR04_2_TRIGGER_PIN, HCSR04_2_ECHO_PIN)
+limiter_switch = Pin(LIMITER_SWITCH_PIN, Pin.IN, Pin.PULL_UP)
 
 
 async def show_what_i_do():
     while True:
         print("Inside distance: %s" % inside_distance.distancecm)
         print("Outside distance: %s" % outside_distance.distancecm)
-        await asyncio.sleep(0.5)
+        print("Limiter switch status: %s" % limiter_switch.value())
+        await asyncio.sleep(1)
 
 
 async def main():
@@ -212,15 +221,19 @@ async def main():
     #  Initialize reading loops
     asyncio.create_task(inside_distance.measure_distance_cm_loop())
     asyncio.create_task(outside_distance.measure_distance_cm_loop())
-    #  Drive curtain up
-    #  await reel_motor.roll_curtain_up()
+    #  Rotate pulley until limiter switch is 1 - wait time 30 seconds
+    # if pulley_motor.curtain_steps = 0, then operation failed to find top position
+    await pulley_motor.find_top_position()
+    print(pulley_motor.curtain_steps)
 
     while True:
-        if inside_distance.distancecm is not None:
+
+        """ if inside_distance.distancecm is not None:
             if (inside_distance.distancecm < 30) and (reel_motor.curtain_up is False):
                 await reel_motor.roll_curtain_up()
             elif (inside_distance.distancecm > 30) and (reel_motor.curtain_up is True):
-                await reel_motor.roll_curtain_down()
-        await asyncio.sleep(0.1)
+                await reel_motor.roll_curtain_down()"""
+
+        await asyncio.sleep(5)
 
 asyncio.run(main())
